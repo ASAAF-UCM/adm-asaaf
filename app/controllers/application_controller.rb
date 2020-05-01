73e92810a4985375f78354b36bfb23edbd3aa1b7
Ismael Aderdor <info@iaderdor.info>
2020-05-01 09:53:13 +0200

    chore: Add view for registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[
+                                        name
+                                        surname1
+                                        surname2
+                                        birthdate
+                                        id_document_type_id
+                                        id_document_expiration_date
+                                        id_document_number
+                                        moodle_name
+                                      ])
+  end
 
   private
 
