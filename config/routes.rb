5bf0ab18e68a163524f0c8004c7acf1eaf54b7eb
Ismael Aderdor <info@iaderdor.info>
2020-05-08 16:19:54 +0200

    chore: Remove devise registration#edit
    We don't want to user devise standard controller and views to change
    user data, but using a personalized controller to do that.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
     get '/:locale' => 'page#home'
-    devise_for :members
+    devise_for :members, skip: [:registrations]
+    as :member do
+      get 'members/new' => 'devise/registrations#new', :as => 'new_member_registration'
+      post 'members' => 'devise/registrations#create', :as => 'member_registration'
+    end
+
     resources :member_administration
 
     resources :role, only: %i[index new create destroy update]
