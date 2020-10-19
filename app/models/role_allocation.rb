36309a58566750d9e56012a87283f1f35e14e6aa
Ismael Aderdor <info@iaderdor.info>
2020-10-19 21:55:35 +0200

    docs: Docs for roles

--- app/models/role_allocation.rb
+++ app/models/role_allocation.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# This model is used to store all the role allocations stored in the database.
+# Basically, each allocation is a pair of a member and the role which has been assigned to the user. Note that a user can have multiple roles at the same time.
 class RoleAllocation < ApplicationRecord
   belongs_to :member
   belongs_to :role_type
--- app/models/role_type.rb
+++ app/models/role_type.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# This model is used to storage the different role names that this app has.
+# Actually, The different roles the app has are (ID | name):
+# - 1 admin
+# - 2 accounting
+# - 3 secretary
+# - 4 loans
 class RoleType < ApplicationRecord
 end
