d2c4e9e0c117a00eecce90a9cf6969f3b4add683
Ismael Aderdor <info@iaderdor.info>
2020-10-26 22:46:52 +0100

    feat: Provides more colors to flash templates

---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module FlashHelper
+def bootstrap_alert(key)
+    case key
+      when 'alert'
+        'warning'
+      when 'notice'
+        'success'
+      when 'error'
+        'danger'
+    end
+  end
+end
