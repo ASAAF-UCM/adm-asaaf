004139ecdcd9d5c554cf55a37056a9bf286fcfb3
Ismael Aderdor <info@iaderdor.info>
2021-02-25 20:14:42 +0100

    fix: Solve issue with locale detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options
-    { locale: I18n.locale }
+  def default_url_options(options = {})
+    options.merge({ locale: I18n.locale })
   end
 end
