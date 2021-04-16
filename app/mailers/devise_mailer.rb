8a0b37ae5bfa1434c2a58d6db89c2180e73ea852
Ismael Aderdor <info@iaderdor.info>
2021-04-16 19:33:57 +0200

    feat: Add simple layout for emails
    This commit closes [#96](https://tree.taiga.io/project/iaderdor-web-administrativa-de-asaaf/task/96)

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,8 +2,16 @@
 
 class DeviseMailer < Devise::Mailer
   helper MailerHelper
+  layout 'mailer'
+
+  before_action :add_inline_attachment!
 
   def subject_for(key)
     t "views.devise.mailer.#{key}.subject"
+  end
+
+  def add_inline_attachment!
+    attachments.inline['logo.png'] =
+      File.read Rails.root.join('app/javascript/images/logo_asaaf.png')
   end
 end
